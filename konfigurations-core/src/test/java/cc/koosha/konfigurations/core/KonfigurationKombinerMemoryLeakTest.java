8d30a58644fda8960ff99b3c34b4d37cc73b1b57
Koosha <info@koosha.cc>
2016-07-07 23:40:44 +0430

    test: make sure no hard reference to observers is kept.

--- konfigurations-core/src/test/java/cc/koosha/konfigurations/core/KonfigurationKombinerMemoryLeakTest.java
+++ konfigurations-core/src/test/java/cc/koosha/konfigurations/core/KonfigurationKombinerMemoryLeakTest.java
@@ -1,5 +1,51 @@
 package cc.koosha.konfigurations.core;
 
-public class KonfigurationKombinerMemoryLeakTest {
+import lombok.val;
+import org.testng.annotations.Test;
 
+import java.util.Collection;
+import java.util.WeakHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+
+public class KonfigurationKombinerMemoryLeakTest extends KonfigurationKombinerBaseTest {
+
+    private final AtomicBoolean run = new AtomicBoolean(false);
+
+    @Test
+    public void testMemLeak() throws Exception {
+
+        val f = KonfigurationKombiner.class.getDeclaredField("observers");
+        f.setAccessible(true);
+        @SuppressWarnings("unchecked")
+        val observers = (WeakHashMap<KeyObserver, Collection<String>>) f.get(kk);
+
+        KeyObserver obs = new KeyObserver() {
+            @Override
+            public void accept(final String key) {
+
+            }
+
+            @Override
+            protected void finalize() throws Throwable {
+                run.set(true);
+                super.finalize();
+            }
+        };
+
+        kk.string("str key").register(obs);
+        assertEquals(observers.size(), 1);
+
+        obs = null;
+
+        System.runFinalization();
+        System.gc();
+        System.runFinalization();
+        System.gc();
+
+        assertTrue(run.get());
+    }
 }
--- konfigurations-core/src/test/java/cc/koosha/konfigurations/core/KonfigurationKombinerTest.java
+++ konfigurations-core/src/test/java/cc/koosha/konfigurations/core/KonfigurationKombinerTest.java
@@ -1,99 +1,13 @@
 package cc.koosha.konfigurations.core;
 
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import static cc.koosha.konfigurations.core.DummyV.dummy;
 import static org.testng.Assert.*;
 
 
-public class KonfigurationKombinerTest {
-
-    private final String str0 = "something else";
-    private final String str1 ="something else appended";
-    private final List<Integer> l0 = Arrays.asList(0, 1, 2, Integer.MAX_VALUE, Integer.MAX_VALUE);
-    private final List<Integer> l1 = Arrays.asList(42, 43, 434);
-
-    private final Map<String, Object> sample0 = map(
-            "str key", dummy(str0),
-            "list key", dummy(l0),
-            "int key", dummy(42)
-    );
-
-    private final Map<String, Object> sample1 = map(
-            "str key", dummy(str1),
-            "list key", dummy(l1),
-            "int key", dummy(42)
-    );
-
-    private Map<String, Object> sample;
-    private String updatedKey0;
-    private String updatedKey1;
-
-    private Map<String, Object> sample() {
-
-        return this.sample;
-    }
-
-    @SuppressWarnings("unchecked")
-    private final Konfiguration dummyKonfig = new Konfiguration() {
-        @Override
-        public KonfigV<Boolean> bool(final String key) {
-            return (KonfigV<Boolean>) sample().get(key);
-        }
-
-        @Override
-        public KonfigV<Integer> int_(final String key) {
-            return (KonfigV<Integer>) sample().get(key);
-        }
-
-        @Override
-        public KonfigV<Long> long_(final String key) {
-            return (KonfigV<Long>) sample().get(key);
-        }
-
-        @Override
-        public KonfigV<String> string(final String key) {
-            return (KonfigV<String>) sample().get(key);
-        }
-
-        @Override
-        public <T> KonfigV<List<T>> list(final String key, final Class<T> type) {
-            return (KonfigV<List<T>>) sample().get(key);
-        }
-
-        @Override
-        public <T> KonfigV<Map<String, T>> map(final String key, final Class<T> type) {
-            return (KonfigV<Map<String, T>>) sample().get(key);
-        }
-
-        @Override
-        public <T> KonfigV<T> custom(final String key, final Class<T> type) {
-            return (KonfigV<T>) sample().get(key);
-        }
-
-        @Override
-        public boolean update() {
-            return true;
-        }
-    };
-
-    private final KonfigurationKombiner kk = new KonfigurationKombiner(dummyKonfig);
-
-
-    @BeforeMethod
-    public void setup() {
-
-        updatedKey0 = null;
-        updatedKey1 = null;
-        sample = sample0;
-    }
-
+public class KonfigurationKombinerTest extends KonfigurationKombinerBaseTest {
 
     @Test
     public void testUpdate() throws Exception {
@@ -120,20 +34,6 @@ public class KonfigurationKombinerTest {
         assertEquals(string.v(), str1);
         assertEquals(list.v(), l1);
         assertEquals(i.v(), (Integer) 42);
-    }
-
-    @SuppressWarnings({"unchecked", "Duplicates"})
-    protected static <T> Map<String, T> map(final String k0, final T v0, final Object... os) {
-
-        assertEquals(os.length % 2, 0);
-
-        final Map<String, T> m = new HashMap<>();
-        m.put(k0, v0);
-
-        for (int i = 0; i < os.length; i+=2)
-            m.put(((String) os[i]), ((T) os[i + 1]));
-
-        return m;
     }
 
 }
