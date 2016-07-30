dc9fae55f6be1dc6c7b6808fa54a112269f8ac17
Koosha <info@koosha.cc>
2016-07-30 22:25:21 +0430

    proper imlementation for DummyV

--- konfigurations-core/src/main/java/cc/koosha/konfigurations/core/DummyV.java
+++ konfigurations-core/src/main/java/cc/koosha/konfigurations/core/DummyV.java
@@ -3,61 +3,85 @@ package cc.koosha.konfigurations.core;
 
 public final class DummyV<T> implements KonfigV<T> {
 
+    private final String key;
     private final T v;
 
-    public DummyV(final T v) {
+    public DummyV(final String key, final T v) {
+        this.key = key;
         this.v = v;
     }
 
     @SuppressWarnings("unchecked")
+    public static <T> DummyV<T> dummy(final String key, final Object v) {
+
+        return new DummyV<>(key, (T) v);
+    }
+
+    public DummyV(final T v) {
+
+        this("", v);
+    }
+
     public static <T> DummyV<T> dummy(final Object v) {
 
-        return new DummyV<>((T) v);
+        return dummy("", v);
     }
+
 
     @Override
     public KonfigV<T> deRegister(final KeyObserver observer) {
-        throw new UnsupportedOperationException();
+
+        return this;
     }
 
     @Override
     public KonfigV<T> register(final KeyObserver observer) {
-        throw new UnsupportedOperationException();
+
+        return this;
     }
 
     @Override
     public KonfigV<T> deRegister(final SimpleObserver observer) {
-        throw new UnsupportedOperationException();
+
+        return this;
     }
 
     @Override
     public KonfigV<T> register(final SimpleObserver observer) {
-        throw new UnsupportedOperationException();
+
+        return this;
     }
 
     @Override
     public KonfigV<T> registerAndCall(final KeyObserver observer) {
-        throw new UnsupportedOperationException();
+
+        observer.accept(this.key());
+        return this;
     }
 
     @Override
     public KonfigV<T> registerAndCall(final SimpleObserver observer) {
-        throw new UnsupportedOperationException();
+
+        observer.accept();
+        return this;
     }
 
     @Override
     public String key() {
-        throw new UnsupportedOperationException();
+
+        return this.key;
     }
 
     @Override
     public T v() {
+
         return this.v;
     }
 
     @Override
     public T v(final T defaultValue) {
-        throw new UnsupportedOperationException();
+
+        return this.v;
     }
 
 }
