bde3f5ea83b42c52872f1ae4dfb874cf3bc946c4
Koosha <info@koosha.cc>
2016-08-11 14:01:20 +0430

    added prefedined constant dummy values.

--- konfigurations-core/src/main/java/cc/koosha/konfigurations/core/DummyV.java
+++ konfigurations-core/src/main/java/cc/koosha/konfigurations/core/DummyV.java
@@ -1,6 +1,9 @@
 package cc.koosha.konfigurations.core;
 
 
+import java.util.*;
+
+
 public final class DummyV<T> implements KonfigV<T> {
 
     private final String key;
@@ -82,6 +85,59 @@ public final class DummyV<T> implements KonfigV<T> {
     public T v(final T defaultValue) {
 
         return this.v;
+    }
+
+
+    // ________________________________________________ PREDEFINED CONST VALUES
+
+    public static final KonfigV<Boolean> FALSE = new DummyV<>(false);
+
+    public static final KonfigV<Boolean> TRUE = new DummyV<>(true);
+
+
+    public static final KonfigV<Integer> INT_0 = new DummyV<>(0);
+
+    public static final KonfigV<Integer> INT_MAX = new DummyV<>(Integer.MAX_VALUE);
+
+    public static final KonfigV<Integer> INT_MIN = new DummyV<>(Integer.MIN_VALUE);
+
+
+    public static final KonfigV<Long> LONG_0 = new DummyV<>(0L);
+
+    public static final KonfigV<Long> LONG_MAX = new DummyV<>(Long.MAX_VALUE);
+
+    public static final KonfigV<Long> LONG_MIN = new DummyV<>(Long.MIN_VALUE);
+
+
+    public static final KonfigV<Double> DOUBLE_0 = new DummyV<>(0.);
+
+    public static final KonfigV<Double> DOUBLE_MAX = new DummyV<>(Double.MAX_VALUE);
+
+    public static final KonfigV<Double> DOUBLE_MIN = new DummyV<>(Double.MIN_VALUE);
+
+    public static final KonfigV<String> EMPTY_STR = new DummyV<>("");
+
+    public static final KonfigV<?> NULL = new DummyV<>(null);
+
+
+    public static <T> DummyV<Collection<T>> emptyCollection() {
+
+        return new DummyV<>(Collections.emptyList());
+    }
+
+    public static <T> DummyV<List<T>> emptyList() {
+
+        return new DummyV<>(Collections.emptyList());
+    }
+
+    public static <K, V> DummyV<Map<K, V>> emptyMap() {
+
+        return new DummyV<>(Collections.emptyMap());
+    }
+
+    public static <T> DummyV<Set<T>> emptySet() {
+
+        return new DummyV<>(Collections.emptySet());
     }
 
 }
